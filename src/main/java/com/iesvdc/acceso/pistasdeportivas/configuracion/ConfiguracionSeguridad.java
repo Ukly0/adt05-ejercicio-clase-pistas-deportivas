9611d9f9a691278995030a67039c81a0b08ccc29
PORTATIL_UKIYO\aexpo <aeg.expositogarcia@gmail.com>
2024-12-11 17:00:35 +0100

    Update Java version to 17 and add OAuth2 client support for GitHub login

--- src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiguracionSeguridad.java
+++ src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiguracionSeguridad.java
@@ -9,78 +9,97 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionSeguridad {
-    @Autowired 
-      DataSource dataSource;
-      
-      @Autowired
-      public void configure(AuthenticationManagerBuilder amb) throws Exception {
-        amb.jdbcAuthentication()
-            .dataSource(dataSource)
-            .usersByUsernameQuery("select username, password, enabled "+
-                "from usuario where username = ?")
-            .authoritiesByUsernameQuery("select u.username, u.tipo as 'authority' "+
-                "from usuario u where username = ?" );
-      }
+        @Autowired
+        DataSource dataSource;
 
-      @Bean
-      BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-      }
+        @Autowired
+        public void configure(AuthenticationManagerBuilder amb) throws Exception {
+                amb.jdbcAuthentication()
+                                .dataSource(dataSource)
+                                .usersByUsernameQuery("select username, password, enabled " +
+                                                "from usuario where username = ?")
+                                .authoritiesByUsernameQuery("select u.username, u.tipo as 'authority' " +
+                                                "from usuario u where username = ?");
+        }
 
-      @Bean
+        @Bean
+        BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
+        @Bean
         public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-                
+
                 // Con Spring Security 6.2 y 7: usando Lambda DSL
 
                 return http
-                        .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/webjars/**", "/img/**", "/css/**", "/js/**", 
-                                        "/register/**", "/ayuda/**", "/login", 
-                                        "/denegado", "/error", "/acerca")
-                                .permitAll() 
-                                .requestMatchers("/admin/**", "/admin/*/**" , "/admin/*/*/**", "/admin/*/*/*/*/*/**",
-                                    "/horario/**", "/horario/*/**" , "/horario/*/*/**", "/horario/*/*/*/*/*/**",
-                                    "/instalacion/**", "/instalacion/*/**" , "/instalacion/*/*/**", "/instalacion/*/*/*/*/*/**")
-                                //.authenticated()
-                                .hasAuthority("ADMIN")
-                                .requestMatchers("/pedidos/**", "/pedidos/*/**", "/pedidos/*/*/**", "/pedidos/*/*/*/**", "/pedidos/*/*/*/*/**")
-                                //.authenticated()
-                                .hasAuthority("OPERARIO")
-                                .requestMatchers("/mis-reservas/**", "/mis-reservas/*/**", 
-                                    "/reservar/**", "/reservar/*/**",
-                                    "/mis-datos/**", "/mis-datos/*/**","/mis-datos/*/*/**","/mis-datos/*/*/*/**")
-                                //.authenticated()
-                                .hasAuthority("USUARIO")
-                        //        .anyRequest().permitAll()
-                        // ).headers(headers -> headers
-                        //         .frameOptions(frameOptions -> frameOptions
-                        //                 .sameOrigin())
-                        // ).sessionManagement((session) -> session
-                        //         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                        ).exceptionHandling((exception)-> exception.
-                                accessDeniedPage("/denegado") )
-                        .formLogin((formLogin) -> formLogin
-                                .loginPage("/login")
-                                //.defaultSuccessUrl("/")
-                                .permitAll()
-                        ).rememberMe(
-                                Customizer.withDefaults()
-                        ).logout((logout) -> logout
-                                .invalidateHttpSession(true)
-                                .logoutSuccessUrl("/")
-                                // .deleteCookies("JSESSIONID") // no es necesario, JSESSIONID se hace por defecto
-                                .permitAll()                                
-                        ).csrf((protection) -> protection
-                                 .disable()
-                        // ).cors((protection)-> protection
-                        //          .disable()
-                        ).build();
+                                .authorizeHttpRequests((requests) -> requests
+                                                .requestMatchers("/webjars/**", "/img/**", "/css/**", "/js/**",
+                                                                "/register/**", "/ayuda/**", "/login",
+                                                                "/denegado", "/error", "/acerca")
+                                                .permitAll()
+                                                .requestMatchers("/horario", "/instalacion")
+                                                .authenticated()
+                                                .requestMatchers("/admin/**", "/admin/*/**", "/admin/*/*/**",
+                                                                "/admin/*/*/*/*/*/**",
+                                                                "/horario/*/**", "/horario/*/*/**",
+                                                                "/horario/*/*/*/*/*/**",
+                                                                "/instalacion/*/**", "/instalacion/*/*/**",
+                                                                "/instalacion/*/*/*/*/*/**")
+                                                // .authenticated()
+                                                .hasAuthority("ADMIN")
+                                                .requestMatchers("/pedidos/**", "/pedidos/*/**", "/pedidos/*/*/**",
+                                                                "/pedidos/*/*/*/**", "/pedidos/*/*/*/*/**")
+                                                // .authenticated()
+                                                .hasAuthority("OPERARIO")
+                                                .requestMatchers("/mis-reservas/**", "/mis-reservas/*/**",
+                                                                "/reservar/**", "/reservar/*/**",
+                                                                "/mis-datos/**", "/mis-datos/*/**", "/mis-datos/*/*/**",
+                                                                "/mis-datos/*/*/*/**")
+                                                // .authenticated()
+                                                .hasAuthority("USUARIO")
+                                // .anyRequest().permitAll()
+                                // ).headers(headers -> headers
+                                // .frameOptions(frameOptions -> frameOptions
+                                // .sameOrigin())
+                                // ).sessionManagement((session) -> session
+                                // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                                ).exceptionHandling((exception) -> exception.accessDeniedPage("/denegado"))
+                                .formLogin((formLogin) -> formLogin
+                                                .loginPage("/login")
+                                                // .defaultSuccessUrl("/")
+                                                .permitAll())
+                                .oauth2Login((oauthlogin) -> oauthlogin
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/")
+                                                .permitAll())
+                                // ).rememberMe(
+                                // Customizer.withDefaults()
+                                .logout((logout) -> logout
+                                                .invalidateHttpSession(true)
+                                                .logoutSuccessUrl("/")
+                                                // .deleteCookies("JSESSIONID") // no es necesario, JSESSIONID se hace
+                                                // por defecto
+                                                .permitAll())
+                                .csrf((protection) -> protection
+                                                .disable()
+                                // ).cors((protection)-> protection
+                                // .disable()
+                                ).build();
 
         }
+
 }
